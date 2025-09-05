4abf2311af9f3991225c02001efed91a17bdfffb
phelan <phelan.minhtanly@gmail.com>
2025-09-05 11:08:43 +0700

    setup react

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+		crypto: "crypto-browserify",
+
+    },
+  },
+  build: {
+    target: "esnext",
+    minify: "esbuild",
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        assetFileNames: (assetInfo: any) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        manualChunks: {
+          react: ["react", "react-dom"],
+          utils: ["lodash", "dayjs"],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom", 'crypto-browserify'],
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
